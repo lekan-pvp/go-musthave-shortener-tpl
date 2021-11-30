c147d7f7efe0f2beed380c3a8bf13567e384a339
lekan <lekan.lekan@gmail.com>
2021-11-30 15:44:24 +0300

    increment 4

--- internal/shortener/handler.go
+++ internal/shortener/handler.go
@@ -1,6 +1,7 @@
 package shortener
 
 import (
+	"encoding/json"
 	"github.com/go-chi/chi/v5"
 	"github.com/lekan-pvp/go-musthave-shortener-tpl/internal/handlers"
 	"io"
@@ -13,6 +14,7 @@ var _ handlers.Handler = &handler{}
 const (
 	createURL = "/"
 	getURL = "/{articleID}"
+	apiShorten = "/api/shorten"
 )
 
 type handler struct {
@@ -27,12 +29,14 @@ func NewHandler() handlers.Handler {
 func (h *handler) Register(router chi.Router) {
 	router.Post(createURL, h.CreateShortURLHandler)
 	router.Route(getURL, func(r chi.Router) {
-		r.Get("/", h.GetURLByID)
+		r.Get("/", h.GetURLByIDHandler)
 	})
+	router.Post(apiShorten, h.ApiShortenHandler)
 }
 
 
-func (h *handler) GetURLByID(w http.ResponseWriter, r *http.Request) {
+// GetURLByIDHandler -- возвращает длинный URL из локального хранилища по ключу, которым является короткий URL
+func (h *handler) GetURLByIDHandler(w http.ResponseWriter, r *http.Request) {
 	articleID := chi.URLParam(r, "articleID")
 	key := prefix +articleID
 
@@ -47,7 +51,7 @@ func (h *handler) GetURLByID(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusTemporaryRedirect)
 }
 
-
+// GetURLByIDHandler -- создает короткий URL и сохраняет в локальном хранилище
 func (h *handler) CreateShortURLHandler(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
 	body, err := io.ReadAll(r.Body)
@@ -68,4 +72,28 @@ func (h *handler) CreateShortURLHandler(w http.ResponseWriter, r *http.Request) 
 	w.Write([]byte(short))
 }
 
+type ApiShortenBody struct {
+	URL string `json:"-"`
+	Result string `json:"result,omitempty"`
+}
 
+// ApiShortenHandler -- принимает и возвращает объекты JSON в теле запроса и ответа
+func (h *handler) ApiShortenHandler(w http.ResponseWriter, r *http.Request)  {
+	var v ApiShortenBody
+	defer r.Body.Close()
+
+	if err := json.NewDecoder(r.Body).Decode(&v); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(http.StatusOK)
+
+	short := h.store.Put(v.URL)
+
+	v.Result = short
+
+	if err := json.NewEncoder(w).Encode(v); err != nil {
+		panic(err)
+	}
+}
