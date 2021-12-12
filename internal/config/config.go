a448ab826727f335d4823c4032ea5b30d1205bbe
lekan <lekan@list.ru>
2021-12-12 21:42:29 +0300

    back to old config

--- internal/config/config.go
+++ internal/config/config.go
@@ -12,21 +12,23 @@ type Config struct {
 	FileStoragePath string `env:"FILE_STORAGE_PATH" envDefault:"short_urls.json"`
 }
 
-var cfg Config
-
-func init() {
-	log.Println("init configuration...")
-	cfg = Config{}
-	if err := env.Parse(&cfg); err != nil {
-		log.Fatal(err)
-	}
-	flag.StringVar(&cfg.ServerAddress, "a", "localhost:8080", "адрес запуска HTTP-сервера")
-	flag.StringVar(&cfg.BaseURL, "b", "http://localhost:8080", "базовый адрес результирующего сокращённого URL")
-	flag.StringVar(&cfg.FileStoragePath, "f", "short_url.json", "путь к файлу с сокращёнными URL")
-
-	flag.Parse()
-}
+var instance *Config
 
 func New() *Config {
-	return &cfg
+	instance = &Config{}
+	if err := env.Parse(instance); err != nil {
+		log.Fatal(err)
+	}
+
+	serverAddressPtr := flag.String("a", instance.ServerAddress, "адрес запуска HTTP-сервера")
+	baseURLPtr := flag.String("b", instance.BaseURL, "базовый адрес результирующего сокращённого URL")
+	fileStoragePathPtr := flag.String("f", instance.FileStoragePath, "путь до файла с сокращёнными URL")
+
+	flag.Parse()
+
+	instance.ServerAddress = *serverAddressPtr
+	instance.BaseURL = *baseURLPtr
+	instance.FileStoragePath = *fileStoragePathPtr
+
+	return instance
 }
