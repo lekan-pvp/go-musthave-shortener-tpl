977d53b423f746262d99e139db7c316d4e2e8e08
lekan <lekan.lekan@gmail.com>
2021-12-22 15:57:41 +0300

    incr10 v.1.6

--- internal/repository/DBRepository.go
+++ internal/repository/DBRepository.go
@@ -1,6 +1,9 @@
 package repository
 
-import "context"
+import (
+	"context"
+	_ "github.com/lib/pq"
+)
 
 func (repo *URLsRepository) CheckPingDB(ctx context.Context) error {
 	if err := repo.DB.PingContext(ctx); err != nil {
