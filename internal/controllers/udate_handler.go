4438afd1f25b4753602214d38cf7a3cab23c173a
lekan <lekan.lekan@gmail.com>
2022-01-17 22:26:07 +0300

    incr14 try v.2.3

--- internal/controllers/udate_handler.go
+++ internal/controllers/udate_handler.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"github.com/lekan-pvp/go-musthave-shortener-tpl.git/internal/cookie_handler"
 	"io"
+	"log"
 	"net/http"
 	"strings"
 )
@@ -21,6 +22,7 @@ func (controller *Controller) UpdateHandler(w http.ResponseWriter, r *http.Reque
 
 	values := strings.Split(cookie.Value, ":")
 	if len(values) != 2 {
+		log.Panicln("cookie format error...")
 		http.Error(w, err.Error(), 500)
 		return
 	}
@@ -28,18 +30,21 @@ func (controller *Controller) UpdateHandler(w http.ResponseWriter, r *http.Reque
 
 	body, err := io.ReadAll(r.Body)
 	if err != nil {
+		log.Println("reading body error...")
 		http.Error(w, err.Error(), 500)
 		return
 	}
 
 	err = json.Unmarshal(body, &in)
 	if err != nil {
+		log.Println("decoding json error...")
 		http.Error(w, err.Error(), 500)
 		return
 	}
 
 	err = controller.UpdateURLs(r.Context(), uuid, in)
 	if err != nil {
+		log.Println("update db error")
 		http.Error(w, err.Error(), 500)
 		return
 	}
