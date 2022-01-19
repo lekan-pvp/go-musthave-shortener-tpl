3ba1e8f9ec586b481959b03583c26ba968f260e5
lekan <lekan.lekan@gmail.com>
2022-01-19 09:33:19 +0300

    incr14 v.6.1

--- internal/repository-db/repository-db.go
+++ internal/repository-db/repository-db.go
@@ -223,7 +223,7 @@ func fanOut(inputCh chan string, n int) []chan string {
 	return chs
 }
 
-func newWorker(inputCh <-chan string) (chan string) {
+func newWorker(inputCh <-chan string) chan string {
 	outCh := make(chan string)
 
 	go func() {
@@ -251,10 +251,10 @@ func (s *DBRepository) UpdateURLsRepo(ctx context.Context, uuid string, shortBas
 
 	n := len(shortBases)
 
-	tx, err := db.Begin()
-	if err != nil {
-		return err
-	}
+	//tx, err := db.Begin()
+	//if err != nil {
+	//	return err
+	//}
 
 	if len(shortBases) == 0 {
 		return errors.New("the list of URLs is empty")
@@ -277,28 +277,29 @@ func (s *DBRepository) UpdateURLsRepo(ctx context.Context, uuid string, shortBas
 	}
 
 
-	stmt, err := tx.PrepareContext(ctx, `UPDATE users SET is_deleted='deleted' WHERE user_id=$1 AND short_url=$2`)
-	if err != nil {
-		log.Println("PrepareContext error...")
-		return err
-	}
+	//stmt, err := tx.PrepareContext(ctx, `UPDATE users SET is_deleted='deleted' WHERE user_id=$1 AND short_url=$2`)
+	//if err != nil {
+	//	log.Println("PrepareContext error...")
+	//	return err
+	//}
 
 	for item := range fanIn(workerChs...) {
-		log.Printf("%s", item)
-		if _, err = stmt.ExecContext(ctx, uuid, item); err != nil {
-			if err = tx.Rollback(); err != nil {
-				log.Println("Rollback error...")
-				return err
-			}
-			log.Println("ExecContext error...")
-			return err
-		}
+		log.Println(item)
+		//log.Printf("%s", item)
+		//if _, err = stmt.ExecContext(ctx, uuid, item); err != nil {
+		//	if err = tx.Rollback(); err != nil {
+		//		log.Println("Rollback error...")
+		//		return err
+		//	}
+		//	log.Println("ExecContext error...")
+		//	return err
+		//}
 	}
 
-	if err = tx.Commit(); err != nil {
-		log.Println("Commit error...")
-		return err
-	}
+	//if err = tx.Commit(); err != nil {
+	//	log.Println("Commit error...")
+	//	return err
+	//}
 
 	log.Println("DB is UPDATED")
 
